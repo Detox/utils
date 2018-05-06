161994b40c7a28d3b6b3d965d9acafb5b312e9b0
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2018-05-06 06:15:14 +0300

    Switch to `array-map-set` package for `ArrayMap` and `ArraySet` implementation

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  * @license 0BSD
  */
 (function(){
-  var string2array, array2string, key_aliases, key_strings, key_usages;
+  var string2array, array2string;
   if (typeof Buffer !== 'undefined') {
     /**
      * @param {string} string
@@ -130,151 +130,7 @@
       return console.error(error);
     }
   }
-  key_aliases = new WeakMap;
-  key_strings = new Map;
-  key_usages = new Map;
-  /**
-   * @param {!Uint8Array} key
-   *
-   * @return {!Uint8Array}
-   */
-  function get_unique_key(key){
-    var real_key, key_string;
-    real_key = key_aliases.get(key);
-    /**
-     * Real key is an array with unique contents that appeared first.
-     * If all of the usages were eliminated, some WeakMap can still point to old real key, which is not a real key anymore, which leads to inconsistencies.
-     * In order to resolve this we have an additional check that confirms if real key is still believed to be a real key.
-     */
-    if (real_key && key_usages.has(real_key)) {
-      return real_key;
-    } else {
-      key_string = key.join(',');
-      if (key_strings.has(key_string)) {
-        real_key = key_strings.get(key_string);
-        key_aliases.set(key, real_key);
-        return real_key;
-      } else {
-        return key;
-      }
-    }
-  }
-  /**
-   * @param {!Uint8Array} key
-   */
-  function increase_key_usage(key){
-    var key_string, current_value;
-    key_string = key.join(',');
-    current_value = key_usages.get(key);
-    if (!current_value) {
-      key_aliases.set(key, key);
-      key_strings.set(key_string, key);
-      key_usages.set(key, 1);
-    } else {
-      ++current_value;
-      key_usages.set(key, current_value);
-    }
-  }
-  /**
-   * @param {!Uint8Array} key
-   */
-  function decrease_key_usage(key){
-    var key_string, current_value;
-    key_string = key.join(',');
-    current_value = key_usages.get(key);
-    --current_value;
-    if (!current_value) {
-      key_strings['delete'](key_string);
-      key_usages['delete'](key);
-    } else {
-      key_usages.set(key, current_value);
-    }
-  }
-  /**
-   * This is a Map with very interesting property: different arrays with the same contents will be treated as the same array
-   *
-   * Implementation keeps weak references to make the whole thing fast and efficient
-   */
-  function ArrayMap(array){
-    var x$, map, i$, len$, ref$, key, value;
-    x$ = map = new Map;
-    x$.get = function(key){
-      key = get_unique_key(key);
-      return Map.prototype.get.call(this, key);
-    };
-    x$.has = function(key){
-      key = get_unique_key(key);
-      return Map.prototype.has.call(this, key);
-    };
-    x$.set = function(key, value){
-      key = get_unique_key(key);
-      if (!Map.prototype.has.call(this, key)) {
-        increase_key_usage(key);
-      }
-      return Map.prototype.set.call(this, key, value);
-    };
-    x$['delete'] = function(key){
-      key = get_unique_key(key);
-      if (Map.prototype.has.call(this, key)) {
-        decrease_key_usage(key);
-      }
-      return Map.prototype['delete'].call(this, key);
-    };
-    x$.clear = function(){
-      var this$ = this;
-      this.forEach(function(arg$, key){
-        this$['delete'](key);
-      });
-    };
-    if (array) {
-      for (i$ = 0, len$ = array.length; i$ < len$; ++i$) {
-        ref$ = array[i$], key = ref$[0], value = ref$[1];
-        map.set(key, value);
-      }
-    }
-    return map;
-  }
-  /**
-   * This is a Set with very interesting property: different arrays with the same contents will be treated as the same array
-   *
-   * Implementation keeps weak references to make the whole thing fast and efficient
-   */
-  function ArraySet(array){
-    var x$, set, i$, len$, item;
-    x$ = set = new Set;
-    x$.has = function(key){
-      key = get_unique_key(key);
-      return Set.prototype.has.call(this, key);
-    };
-    x$.add = function(key){
-      key = get_unique_key(key);
-      if (!Set.prototype.has.call(this, key)) {
-        increase_key_usage(key);
-      }
-      return Set.prototype.add.call(this, key);
-    };
-    x$['delete'] = function(key){
-      key = get_unique_key(key);
-      if (Set.prototype.has.call(this, key)) {
-        decrease_key_usage(key);
-      }
-      return Set.prototype['delete'].call(this, key);
-    };
-    x$.clear = function(){
-      var this$ = this;
-      this.forEach(function(arg$, key){
-        this$['delete'](key);
-      });
-    };
-    if (array) {
-      for (i$ = 0, len$ = array.length; i$ < len$; ++i$) {
-        item = array[i$];
-        set.add(item);
-      }
-    }
-    return set;
-  }
-  function Wrapper(detoxBaseX, randomBytesNumbers){
+  function Wrapper(detoxBaseX, arrayMapSet, randomBytesNumbers){
     var random_bytes, random_int, random, base58;
     random_bytes = randomBytesNumbers['random_bytes'];
     random_int = randomBytesNumbers['random_int'];
@@ -321,17 +177,17 @@
       'timeoutSet': timeoutSet,
       'intervalSet': intervalSet,
       'error_handler': error_handler,
-      'ArrayMap': ArrayMap,
-      'ArraySet': ArraySet,
+      'ArrayMap': arrayMapSet['ArrayMap'],
+      'ArraySet': arrayMapSet['ArraySet'],
       'base58_encode': base58['encode'],
       'base58_decode': base58['decode']
     };
   }
   if (typeof define === 'function' && define['amd']) {
-    define(['@detox/base-x', 'random-bytes-numbers'], Wrapper);
+    define(['@detox/base-x', 'array-map-set', 'random-bytes-numbers'], Wrapper);
   } else if (typeof exports === 'object') {
-    module.exports = Wrapper(require('@detox/base-x'), require('random-bytes-numbers'));
+    module.exports = Wrapper(require('@detox/base-x'), require('array-map-set'), require('random-bytes-numbers'));
   } else {
-    this['detox_utils'] = Wrapper(this['base_x'], this['random_bytes_numbers']);
+    this['detox_utils'] = Wrapper(this['base_x'], this['array_map_set'], this['random_bytes_numbers']);
   }
 }).call(this);
--- src/index.ls
+++ src/index.ls
@@ -101,121 +101,7 @@ function error_handler (error)
 	if error instanceof Error
 		console.error(error)
 
-key_aliases	= new WeakMap
-key_strings	= new Map
-key_usages	= new Map
-/**
- * @param {!Uint8Array} key
- *
- * @return {!Uint8Array}
- */
-function get_unique_key (key)
-	real_key = key_aliases.get(key)
-	/**
-	 * Real key is an array with unique contents that appeared first.
-	 * If all of the usages were eliminated, some WeakMap can still point to old real key, which is not a real key anymore, which leads to inconsistencies.
-	 * In order to resolve this we have an additional check that confirms if real key is still believed to be a real key.
-	 */
-	if real_key && key_usages.has(real_key)
-		real_key
-	else
-		key_string	= key.join(',')
-		# If real key already exists, use it and create alias
-		if key_strings.has(key_string)
-			real_key	= key_strings.get(key_string)
-			key_aliases.set(key, real_key)
-			real_key
-		else
-			key
-/**
- * @param {!Uint8Array} key
- */
-!function increase_key_usage (key)
-	key_string		= key.join(',')
-	current_value	= key_usages.get(key)
-	# On first use of real key create WeakMap alias to itself and string alias, set number of usages to 1
-	if !current_value
-		key_aliases.set(key, key)
-		key_strings.set(key_string, key)
-		key_usages.set(key, 1)
-	else
-		++current_value
-		key_usages.set(key, current_value)
-/**
- * @param {!Uint8Array} key
- */
-!function decrease_key_usage (key)
-	key_string		= key.join(',')
-	current_value	= key_usages.get(key)
-	--current_value
-	# When last usage was eliminated, clean string alias and usages, WeakMap will clean itself over time or upon request (we can't enumerate its keys ourselves)
-	if !current_value
-		key_strings.delete(key_string)
-		key_usages.delete(key)
-	else
-		key_usages.set(key, current_value)
-
-# LiveScript doesn't support classes, so we do it in ugly way
-/**
- * This is a Map with very interesting property: different arrays with the same contents will be treated as the same array
- *
- * Implementation keeps weak references to make the whole thing fast and efficient
- */
-function ArrayMap (array)
-	map = new Map
-		..get = (key) ->
-			key	= get_unique_key(key)
-			Map::get.call(@, key)
-		..has	= (key) ->
-			key	= get_unique_key(key)
-			Map::has.call(@, key)
-		..set = (key, value) ->
-			key	= get_unique_key(key)
-			if !Map::has.call(@, key)
-				increase_key_usage(key)
-			Map::set.call(@, key, value)
-		..delete = (key) ->
-			key	= get_unique_key(key)
-			if Map::has.call(@, key)
-				decrease_key_usage(key)
-			Map::delete.call(@, key)
-		..clear = !->
-			@forEach (, key) !~>
-				@delete(key)
-	if array
-		for [key, value] in array
-			map.set(key, value)
-	map
-# LiveScript doesn't support classes, so we do it in ugly way
-/**
- * This is a Set with very interesting property: different arrays with the same contents will be treated as the same array
- *
- * Implementation keeps weak references to make the whole thing fast and efficient
- */
-function ArraySet (array)
-	set	= new Set
-		..has	= (key) ->
-			key	= get_unique_key(key)
-			Set::has.call(@, key)
-		..add = (key) ->
-			key	= get_unique_key(key)
-			if !Set::has.call(@, key)
-				increase_key_usage(key)
-			Set::add.call(@, key)
-		..delete = (key) ->
-			key	= get_unique_key(key)
-			if Set::has.call(@, key)
-				decrease_key_usage(key)
-			Set::delete.call(@, key)
-		..clear = !->
-			@forEach (, key) !~>
-				@delete(key)
-	if array
-		for item in array
-			set.add(item)
-	set
-
-function Wrapper (detox-base-x, random-bytes-numbers)
+function Wrapper (detox-base-x, array-map-set, random-bytes-numbers)
 	random_bytes	= random-bytes-numbers['random_bytes']
 	random_int		= random-bytes-numbers['random_int']
 	random			= random-bytes-numbers['random']
@@ -260,18 +146,18 @@ function Wrapper (detox-base-x, random-bytes-numbers)
 		'timeoutSet'					: timeoutSet
 		'intervalSet'					: intervalSet
 		'error_handler'					: error_handler
-		'ArrayMap'						: ArrayMap
-		'ArraySet'						: ArraySet
+		'ArrayMap'						: array-map-set['ArrayMap']
+		'ArraySet'						: array-map-set['ArraySet']
 		'base58_encode'					: base58['encode']
 		'base58_decode'					: base58['decode']
 	}
 
 if typeof define == 'function' && define['amd']
 	# AMD
-	define(['@detox/base-x', 'random-bytes-numbers'], Wrapper)
+	define(['@detox/base-x', 'array-map-set', 'random-bytes-numbers'], Wrapper)
 else if typeof exports == 'object'
 	# CommonJS
-	module.exports = Wrapper(require('@detox/base-x'), require('random-bytes-numbers'))
+	module.exports = Wrapper(require('@detox/base-x'), require('array-map-set'), require('random-bytes-numbers'))
 else
 	# Browser globals
-	@'detox_utils' = Wrapper(@'base_x', @'random_bytes_numbers')
+	@'detox_utils' = Wrapper(@'base_x', @'array_map_set', @'random_bytes_numbers')
